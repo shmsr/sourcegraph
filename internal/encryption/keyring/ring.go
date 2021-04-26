000bc77074e04c1e58ba1fa7c2ebb582309564e5
Erik Seliger <erikseliger@me.com>
2021-04-26 18:11:31 +0200

    Implement AWS KMS secret encryption backend

--- internal/encryption/keyring/ring.go
+++ internal/encryption/keyring/ring.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/encryption"
+	"github.com/sourcegraph/sourcegraph/internal/encryption/awskms"
 	"github.com/sourcegraph/sourcegraph/internal/encryption/cloudkms"
 	"github.com/sourcegraph/sourcegraph/internal/encryption/mounted"
 	"github.com/sourcegraph/sourcegraph/schema"
@@ -116,6 +117,8 @@ func NewKey(ctx context.Context, k *schema.EncryptionKey) (encryption.Key, error
 	switch {
 	case k.Cloudkms != nil:
 		return cloudkms.NewKey(ctx, *k.Cloudkms)
+	case k.Awskms != nil:
+		return awskms.NewKey(ctx, *k.Awskms)
 	case k.Mounted != nil:
 		return mounted.NewKey(ctx, *k.Mounted)
 	case k.Noop != nil:
